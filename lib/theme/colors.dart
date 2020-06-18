f3ae82d060522a61edbe0af889c85ffb8abb427b
Junihor Moran <jmoran@belatrixsf.com>
2020-06-18 13:34:40 -0500

    base colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kYellowColor = Color(0xfff3bf37);
+const kOrangeColor = Color(0xfff58134);
+const kGreenColor = Color(0xff239281);
+const kGreyColor = Color(0xffd0d2d3);
