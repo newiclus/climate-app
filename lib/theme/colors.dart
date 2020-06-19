4365347768d3b245b4989e9c1d171f75759b64b2
Junihor Moran <jmoran@belatrixsf.com>
2020-06-18 19:27:02 -0500

    Add text color to theme

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 const kYellowColor = Color(0xfff3bf37);
 const kOrangeColor = Color(0xfff58134);
 const kGreenColor = Color(0xff239281);
-const kGreyColor = Color(0xffd0d2d3);
+const kGreyColor = Color(0xffeeedef);
+const kTextColor = Color(0xff535254);
